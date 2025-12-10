845df6e17186363e80b1cf4f65f3468b89877532
Blazing <160132339+theblazingolem@users.noreply.github.com>
2025-12-10 15:16:38 +0530

    added quotes

--- data/quotes.js
+++ data/quotes.js
@@ -34,6 +34,51 @@ export const quotes = [
         link: "https://discord.com/channels/841699180271239218/1008659239864115220/1419342251968168038",
         author: "1171095449072046132",
     },
+    {
+        text: "I hope you get deported to isla Sorna",
+        link: "https://discord.com/channels/841699180271239218/1008659239864115220/1436741865499267082",
+        author: "",
+    },
+    {
+        text: "stfu evival, no bitches",
+        link: "https://discord.com/channels/841699180271239218/1008659239864115220/1420637638867816580",
+        author: "",
+    },
+    {
+        text: "Jarvis, white Darius react this man",
+        link: "https://discord.com/channels/841699180271239218/1008659239864115220/1411541249856639079",
+        author: "",
+    },
+    {
+        text: "the only lego i have from jwr, is the 1 by 1 lego piece",
+        link: "https://discord.com/channels/841699180271239218/1008659239864115220/1399670013069365308",
+        author: "",
+    },
+    {
+        text: "big eatie's the hippo rit?",
+        link: "https://discord.com/channels/841699180271239218/1008659239864115220/1375806061759565998",
+        author: "",
+    },
+    {
+        text: "Can we eat people, that are offline?",
+        link: "https://discord.com/channels/841699180271239218/1008659239864115220/1357228094372712518",
+        author: "",
+    },
+    {
+        text: "911 MEMBERS",
+        link: "https://discord.com/channels/841699180271239218/1008659239864115220/1367508619922243585",
+        author: "",
+    },
+    {
+        text: "I miss the time when the server was about dinosaurs",
+        link: "https://discord.com/channels/841699180271239218/1008659239864115220/1373698466559164619",
+        author: "",
+    },
+    {
+        text: "D-Rex makes Quasimodo from the Hunchback of Netro Dame look good",
+        link: "https://discord.com/channels/841699180271239218/1008659239864115220/1391247183356891136",
+        author: "",
+    },
 ];
 /*
     {
